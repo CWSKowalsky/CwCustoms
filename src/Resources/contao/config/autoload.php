b0c74cea28e42f826c9a3fd0ad8890085734e81b
SKowalsky <samuel.kowalsky@freenet.de>
2019-08-02 08:54:40 +0200

    stuff

--- src/Resources/contao/config/autoload.php
+++ src/Resources/contao/config/autoload.php
@@ -4,8 +4,3 @@ ClassLoader::addNamespaces(array
 (
     'SKowalsky',
 ));
-
-ClassLoader::addClasses(array(
-   '\Isotope\CheckoutStep\Address' => 'vendor/skowalsky/cw_customs/src/Resources/contao/classes/Address.php',
-   '\Isotope\CheckoutStep\OrderConditions' => 'vendor/skowalsky/cw_customs/src/Resources/contao/classes/OrderConditions.php'
-));
